a0960c104cbcf00b508822da567867298739b68f
Stéphane <stephane1920@gmail.com>
2022-01-07 18:28:21 +0100

    init project environement webpack

---
+++ README.md
@@ -0,0 +1,47 @@
+
+# Les petits plats : P7 OpenClassrooms	
+![made-with-node](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)  ![made-with-javascript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E) ![made-with-sass](	https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white) ![GitHub repo size](https://img.shields.io/github/repo-size/StephaneLi/StephaneLieumont_6_09122021)
+
+---
+## Contexte 
+
+Vous êtes freelance et vous venez d’être missionné par l’entreprise “Les petits plats” en temps que Développeur Front-end pour une mission de 3 mois.  
+
+1. Analyser un problème informatique
+2. Développer un algorithme pour résoudre un problème
+
+#### Sources
+[JSON data](https://github.com/OpenClassrooms-Student-Center/P11-front-end-search-engine)  
+[Maquette](https://www.figma.com/file/xqeE1ZKlHUWi2Efo8r73NK)  
+[User stories](https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P6+Algorithms/Cas+d%E2%80%99utilisation+%2303+Filtrer+les+recettes+dans+l%E2%80%99interface+utilisateur.pdf)
+
+## Usage 
+Utiliser [node.js](https://nodejs.org/en/download/) pour travailler sur le projet.
+
+Environnement de développement :
+
+```bash
+npm install
+npm run dev
+```
+Environnement de production :
+
+```bash
+npm install
+npm run prod
+```
+
+## Livrables
+- Une fiche d’investigation de fonctionnalité sur l’algorithme de recherche (format PDF). Vous y intégrerez le choix de l'algorithme définitif en comparaison à l’autre algorithme de recherche développé.
+- Un lien vers le repo GitHub du projet comprenant les deux branches avec les deux solutions différentes pour la recherche. 
+
+## Contribution
+Deux branches sont utilisées pour les deux algorythmes different
+search_feature_v1
+search_feature_v2
+
+##  Auteur
+
+Stéphane LIEUMONT
+[Projets OpenClassrooms](https://oc.sli-3d.fr/)
+[Portfolio 3D](https://portfolio.sli-3d.fr/)
---
+++ public/js/app.js
@@ -0,0 +1,60 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/***/ (() => {
+
+eval("console.log('test');//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9sZXNfcGV0aXRzX3BsYXRzLy4vc3JjL2FwcC5qcz8xMTEyIl0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiJBQUNBQSxPQUFPLENBQUNDLEdBQVIsQ0FBWSxNQUFaIiwic291cmNlc0NvbnRlbnQiOlsiXHJcbmNvbnNvbGUubG9nKCd0ZXN0JylcclxuIl0sImZpbGUiOiIuL3NyYy9hcHAuanMuanMiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/app.js\n");
+
+/***/ }),
+
+/***/ "./src/scss/app.scss":
+/*!***************************!*\
+  !*** ./src/scss/app.scss ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2Nzcy9hcHAuc2Nzcy5qcyIsIm1hcHBpbmdzIjoiO0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9sZXNfcGV0aXRzX3BsYXRzLy4vc3JjL3Njc3MvYXBwLnNjc3M/Njk2YiJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/scss/app.scss\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	__webpack_modules__["./src/app.js"](0, {}, __webpack_require__);
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/scss/app.scss"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
